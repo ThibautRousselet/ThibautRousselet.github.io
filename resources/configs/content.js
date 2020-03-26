592c3d1e75421c528ddeb5e3d70aa27afdbeed88
ThibautRousselet <tibo25300@gmail.com>
2020-03-26 23:07:06 +0100

    Initial commit

---
+++ resources/configs/content.js
@@ -0,0 +1,90 @@
+/**
+ * This config file contains the content of the page, i.e. the different
+ * categories and their respective items.
+ */
+const content = [
+  {
+    "categoryName": {
+      "en": "Work in progress",
+      "fr": "Projet en cours"
+    },
+    "content": [
+      {
+        "title": {
+          "en": "Space rogue like",
+          "fr": "Space rogue like"
+        },
+        "description": {
+          "en": `This untitled game will be a shoot them up including rogue like elements such as random weapons that you can equip and mix.<br/>
+                <br/>
+                This is the project of my Games Programming semester. It will be developed by a team of 5 people on Unity.
+                 `,
+          "fr": `Ce jeu sera un shoot them up comprenant des éléments de Rogue Like avec des armes aléatoires à placer sur son vaisseau spatiale.<br/>
+                <br/>
+                Ce projet constitue le rendu final de mon semestre de Games Programming. Il sera développé sous Unity.
+                `
+        },
+        "date": "2020",
+        "imagePath": "projects/project1.png",
+        "link": ""
+      }
+    ]
+  },
+  {
+    "categoryName": {
+      "en": "Realized projects",
+      "fr": "Projets terminés"
+    },
+    "content": [
+        {
+          "title": {
+          "en": "Hazardous job",
+          "fr": "Hazardous job"
+        },
+        "description": {
+          "en": `Hazardous Job is a horror game in VR, where you have to pick up and deliver flasks in a zombie infested complex.<br/><br/>
+                 It was developed by a group of 2 people in a VR class, using unity and an Oculus Rift S. <br/>
+                 `,
+          "fr": `Hazardous Job est un jeu d'horreur en VR, dans lequel vous incarnez un ouvrier contraint de livrer des fioles d'un coté à l'autre d'un complexe infesté de zombies.<br/><br/>
+                 Il a été développé par deux personnes à l'aide d'un Occulus Rift S sous Unity dans le cadre d'un cours de VR.`
+        },
+        "date": "2019",
+        "imagePath": "projects/project2.png",
+        "link": ""
+      },
+      {
+        "title": {
+        "en": "Back To The Catan",
+        "fr": "Back To The Catan"
+      },
+      "description": {
+        "en": `This game is inspired by the famous board game Catan, this version is set in the world of the Back to the Future franchise and contains new gameplay features such as time travel<br/><br/>
+               It was developed by a group of 2 people in Java during an Object Oriented class. <br/>
+               `,
+        "fr": `Back To The Catan est une une version alternative du célèbre jeu de plateau Catan, se déroulant dans l'univers de Retour Vers Le Futur. Elle comprend des mécaniques inédites comme le voyage dans le temps.<br/><br/>
+               le jeu a été implémenté par deux personnes en Java pour un cours de programmation orientée objet.`
+      },
+      "date": "2019",
+      "imagePath": "projects/project35.png",
+      "link": "https://github.com/UTSam/BackToTheCatan"
+    },
+    {
+      "title": {
+      "en": "Teeko Game",
+      "fr": "Jeu du Teeko"
+    },
+    "description": {
+      "en": `Implementation of the Teeko board game, in which you can play against an AI with 3 levels of difficulty.<br/><br/>
+             This project was made in C++ by a group of 4 in a class focused on AI. <br/>
+             `,
+      "fr": `Reproduction du jeu du teeko, offrant la possibilité de jouer contre une IA avec trois niveaux de difficulté.<br/><br/>
+             Ce projet fut réalisé en C++ à 4 dans un cours centré sur l'IA.`
+    },
+    "date": "2018",
+    "imagePath": "projects/project4.png",
+    "link": "https://github.com/YannPoncet/Project-IA41"
+  }
+    ]
+  },
+  
+]
---
+++ resources/configs/general_configs.js
@@ -0,0 +1,16 @@
+/**
+ * This config file contains the general settings of the page, including the
+ * content displayed in the about section
+ */
+const generalConfigs = {
+  "aboutSection": {
+    "me": "Thibaut Rousselet",
+    "image": "me.png",
+    "myDescription": {
+      "fr": "Etudiant Ingénieur Informatique/Jeux Vidéo",
+      "en": "Engineering Student in Computer Science/Video Games"
+    },
+    "githubLink": "https://github.com/ThibautRousselet?tab=stars",
+    "linkedinLink": "coucs",
+  }
+}
